a73099cf577fc1a09f4305aac4ba960930311753
Daniel <danielfernandezmateos03@gmail.com>
2024-04-23 14:13:56 +0200

    Problema MailerService

--- servers/apps/users/src/types/user.types.ts
+++ servers/apps/users/src/types/user.types.ts
@@ -3,27 +3,27 @@ import { User } from '../entities/user.entity';
 
 @ObjectType()
 export class ErrorType {
-    @Field()
-    message: string;
+  @Field()
+  message: string;
 
-    @Field({ nullable: true })
-    code?: string;
+  @Field({ nullable: true })
+  code?: string;
 }
 
 @ObjectType()
-export class RegisterRepose {
-    @Field(() => User, { nullable: true })
-    user?: User | any;
+export class RegisterResponse {
+  @Field(() => User, { nullable: true })
+  user?: User | any;
 
-    @Field(() => ErrorType, { nullable: true })
-    error?: ErrorType;
+  @Field(() => ErrorType, { nullable: true })
+  error?: ErrorType;
 }
 
 @ObjectType()
-export class LoginRepose {
-    @Field(() => User)
-    user: User;
+export class LoginResponse {
+  @Field(() => User)
+  user: User;
 
-    @Field(() => ErrorType, { nullable: true })
-    error?: ErrorType;
+  @Field(() => ErrorType, { nullable: true })
+  error?: ErrorType;
 }
--- servers/apps/users/src/users.module.ts
+++ servers/apps/users/src/users.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../../../prisma/Prisma.Service';
-import { UsersResolver } from './users.resolver';
+import { UsersResolver } from './user.resolver';
 import { EmailModule } from './email/email.module';
 
 @Module({
